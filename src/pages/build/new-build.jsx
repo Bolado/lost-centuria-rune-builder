97f9d4f3d4333872e1830d06f16d400f447ac734
Igor <github@bolado.dev>
2024-10-30 02:44:57 +0100

    new monster data sorted alphabetically the names

--- src/pages/build/new-build.jsx
+++ src/pages/build/new-build.jsx
@@ -15,15 +15,20 @@ function NewBuild() {
                 <div className='basis-1/2 bg-slate-500/10 m-4 p-4 rounded-md'>
                     {selectedMonster ? (
                         <div>
-                            <img src={selectedMonster.icon} alt={selectedMonster.name} className='w-16 h-16' />
-                            <p className='text-lg font-bold'>{selectedMonster.name}</p>
-                            <p>HP: {selectedMonster.hp}</p>
-                            <p>STR: {selectedMonster.str}</p>
-                            <p>ATK: {selectedMonster.atk}</p>
-                            <p>DEF: {selectedMonster.def}</p>
+                            <img src={selectedMonster.icon} alt={selectedMonster.name} className='w-20 h-20 mx-auto' />
+                            <p className='text-xl font-bold text-center'>{selectedMonster.name}</p>
+                            <p>HP: {selectedMonster.baseStats.hp}</p>
+                            <p>ATK: {selectedMonster.baseStats.atk}</p>
+                            <p>DEF: {selectedMonster.baseStats.def}</p>
+                            <p>ACC: {selectedMonster.baseStats.acc}</p>
+                            <p>CDD: {selectedMonster.baseStats.cdd}</p>
+                            <p>CR: {selectedMonster.baseStats.cr}</p>
+                            <p>CD: {selectedMonster.baseStats.cd}</p>
+                            <p>RES: {selectedMonster.baseStats.res}</p>
+                            <p>PEN: {selectedMonster.baseStats.pen}</p>
                         </div>
                     ) : (
-                        <div>select a monster</div>
+                        <div>Select a monster</div>
                     )}
                 </div>
                 <div className='basis-1/2 bg-slate-500/10 m-4 p-4 rounded-md'>
@@ -41,12 +46,14 @@ function NewBuild() {
                 </div>
                 <div className='w-full flex flex-wrap'>
                     {isMonsters ? (
-                        monsters.map((monster) => (
-                            <div key={monster.id} className='m-2 p-2 flex flex-col items-center w-20 text-center' onClick={() => setSelectedMonster(monsters.find((m) => m.id === monster.id))}>
-                                <img src={monster.icon} alt={monster.name} className='w-16 h-16' />
-                                <div>{monster.name}</div>
-                            </div>
-                        ))
+                            monsters
+                            .sort((a, b) => a.name.localeCompare(b.name))
+                            .map((monster) => (
+                                <div key={monster.id} className='m-2 p-2 flex flex-col items-center w-20 text-center cursor-pointer hover:bg-slate-500/20 rounded-md' onClick={() => setSelectedMonster(monsters.find((m) => m.id === monster.id))}>
+                                    <img src={monster.icon} alt={monster.name} className='w-16 h-16' />
+                                    <div>{monster.name}</div>
+                                </div>
+                            ))
                     ) : (
                         <div>Runes</div>
                     )}
